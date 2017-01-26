7eb5e2bba8605a3f448b84be8c28f64ef7110336
[jsw324] <jwalkow@tallience.com>
2017-01-26 14:06:13 -0500

    small change heroku

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ var routes = require('./routes/index');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+//app.set('port', (process.env.PORT || 5000));
 
 app.set('view engine', 'ejs');
 
 app.use('/', routes);
 
-app.listen(app.get('port'), function() {
-  console.log('Node running on port', app.get('port'));
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Node running on port');
 });
 
 module.exports = app;
